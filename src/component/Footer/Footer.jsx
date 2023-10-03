3d4f3c7e6eb31223ffc2b0b4edede28b53de9b47
abdurrahim220 <abdurrahim5@gmail.com>
2023-10-03 10:37:30 +0600

    footer added

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from "react";
+import { AiFillFacebook, AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-8 grid md:grid-cols-5 m-auto items-center justify-center">
+      <div>
+        <div className="logoDiv">
+          <h1 className="logo text-[25px] text-white">
+            <strong>Job</strong>Search
+          </h1>
+        </div>
+        <p className="text-white pb-[13px] opacity-70 leading-7">
+          We always make our seekers and companies find the best jobs and
+          employers find the best candidates
+        </p>
+      </div>
 
-export default Footer
+      <div className="grid">
+        <span className="divTitle text-[18px] font-semibold pb-[1.5rem] text-white">
+          company
+        </span>
+        <div className="grid gap-3">
+          <li className="text-white opacity-[.7] hover:opacity-[1]">About Us</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Features</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">News</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">FAQ</li>
+        </div>
+      </div>
+
+      <div className="grid">
+        <span className="divTitle text-[18px] font-semibold pb-[1.5rem] text-white">
+          Resources
+        </span>
+        <div className="grid gap-3">
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Account</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Support Center</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">FeedBack</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Contact Us</li>
+        </div>
+      </div>
+
+      <div className="grid">
+        <span className="divTitle text-[18px] font-semibold pb-[1.5rem] text-white">
+          Support
+        </span>
+        <div className="grid gap-3">
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Events</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Promo</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Req Demo</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Careers</li>
+        </div>
+      </div>
+
+      <div className="grid">
+        <span className="divTitle text-[18px] font-semibold pb-[1.5rem] text-white">
+         Contact Info
+        </span>
+        <small className="text-white text-[14px]">
+          z.abdurrahim5@gmail.com
+        </small>
+        <div className="icon flex gap-4 py-[1rem]">
+
+          <AiFillInstagram className="bg-white p-[8px] h-[35px] w-[35px] rounded-full icon text-blurColor"/>
+          <AiOutlineTwitter className="bg-white p-[8px] h-[35px] w-[35px] rounded-full icon text-blurColor"/>
+          <AiFillFacebook className="bg-white p-[8px] h-[35px] w-[35px] rounded-full icon text-blurColor"/>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/component/ValueDiv/ValueDiv.jsx
+++ src/component/ValueDiv/ValueDiv.jsx
@@ -55,6 +55,17 @@ const ValueDiv = () => {
 
       </div>
 
+     <div className="mt-[2rem] flex justify-between bg-greyIsh  p-[5rem] rounded-[10px]">
+
+     <div>
+        <h1 className="text-blueColor text-[30px] font-bold">Ready to switch a career</h1>
+        <h1 className="text-black text-[30px] font-bold">Let's get Started!</h1>
+      </div>
+      <button className="border-[2px] rounded-[10px] py-[4px] px-[50px] text-[18px] font-semibold text-blueColor hover:bg-white border-blueColor">
+        Get Started
+      </button>
+     </div>
+
     </div>
   );
 };
