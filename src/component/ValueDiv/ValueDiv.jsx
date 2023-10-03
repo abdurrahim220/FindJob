2c7bb03f235ab6e63641215794f88149c39313a9
abdurrahim220 <abdurrahim5@gmail.com>
2023-10-03 09:35:58 +0600

    value added

--- src/component/ValueDiv/ValueDiv.jsx
+++ src/component/ValueDiv/ValueDiv.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
 
 const ValueDiv = () => {
   return (
-    <div>ValueDiv</div>
-  )
-}
+    <div className="mb-[4rem] mt-[6rem]">
+      <h1 className="text-textColor text-[25px] py-[2rem] pb-[3rem] font-bold w-[600px] block">
+        The Vale that holds us true and to account
+      </h1>
 
-export default ValueDiv
+      <div className="grid gap-[5rem] md:gap-[10rem] grid-cols-1 md:grid-cols-3 items-center">
+
+        <div className="singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
+          <div className="flex items-center gap-3">
+            <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center">
+              <img src="" alt="" className="w-[70%]" />
+            </div>
+            <span className="font-semibold text-textColor text-[18px]">
+              Simplicity
+            </span>
+          </div>
+          <p className="text-[25px] text-textColor opacity-[.7] py-[1rem] font-semibold">
+            Things beinf made beautiful simple are at the heart of everything we
+            do.
+          </p>
+        </div>
+        <div className="singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
+          <div className="flex items-center gap-3">
+            <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center">
+              <img src="" alt="" className="w-[70%]" />
+            </div>
+            <span className="font-semibold text-textColor text-[18px]">
+              Simplicity
+            </span>
+          </div>
+          <p className="text-[25px] text-textColor opacity-[.7] py-[1rem] font-semibold">
+            Things beinf made beautiful simple are at the heart of everything we
+            do.
+          </p>
+        </div>
+        <div className="singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
+          <div className="flex items-center gap-3">
+            <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center">
+              <img src="" alt="" className="w-[70%]" />
+            </div>
+            <span className="font-semibold text-textColor text-[18px]">
+              Simplicity
+            </span>
+          </div>
+          <p className="text-[25px] text-textColor opacity-[.7] py-[1rem] font-semibold">
+            Things beinf made beautiful simple are at the heart of everything we
+            do.
+          </p>
+        </div>
+
+
+      </div>
+
+    </div>
+  );
+};
+
+export default ValueDiv;
